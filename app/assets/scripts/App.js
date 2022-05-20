a2b6efaa77243284baae627244d92a4d7cd6ec4a
cmcoke <minoru.cm@gmail.com>
2022-05-20 18:07:35 -0500

    used webpack.js to perform a code split for the Modal.js file.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,12 +2,25 @@ import "../styles/styles.css";
 import "lazysizes";
 import "picturefill";
 import MobileMenu from "./modules/MobileMenu";
-// import Modal from "./modules/Modal";
 import DonationForm from "./modules/DonationForm";
 
-let mobileMenu = new MobileMenu();
-// let modal = new Modal();
-let donateForm = new DonationForm();
+new MobileMenu();
+new DonationForm();
+
+// performs a code split for the modal.js file
+let modal;
+document.querySelector(".profile__content").addEventListener("click", () => {
+  if (typeof modal == "undefined") {
+    import(/* webpackChunkName: "modal" */ "./modules/Modal")
+      .then(x => {
+        modal = new x.default();
+        setTimeout(() => modal.openModal(), 20);
+      })
+      .catch(() => console.log("there was a problem."));
+  } else {
+    modal.openModal();
+  }
+});
 
 // allows webpack-dev-server to be executed by allowing hot module replacements and accepts updates on the fly
 if (module.hot) {
--- app/assets/scripts/modules/DonationForm.js
+++ app/assets/scripts/modules/DonationForm.js
@@ -1,30 +1,27 @@
 class DonationForm {
   constructor() {
-    this.selected = document.querySelectorAll('.selected');
-    this.donationValue = document.querySelector('.donate__value');
-    this.donateButton = document.querySelector('.donate__submit');
+    this.selected = document.querySelectorAll(".selected");
+    this.donationValue = document.querySelector(".donate__value");
+    this.donateButton = document.querySelector(".donate__submit");
     this.events();
   }
 
   events() {
-
     this.selected.forEach(amount => {
-      amount.addEventListener('click', () => this.result(amount))
-    })
+      amount.addEventListener("click", () => this.result(amount));
+    });
 
-    this.donateButton.addEventListener('click', (e) => this.submitDonation(e))
-
+    this.donateButton.addEventListener("click", e => this.submitDonation(e));
   }
 
-  result(amount){
+  result(amount) {
     this.donationValue.value = amount.value;
   }
 
-  submitDonation(e){
+  submitDonation(e) {
     e.preventDefault();
-    this.donationValue.value = '';
+    this.donationValue.value = "";
   }
-
 }
 
 export default DonationForm;
